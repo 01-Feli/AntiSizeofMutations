a2f6fc840d42c8cb82eab21cbd47526595f1797a
Feli <felinegri5@gmail.com>
2021-02-04 16:16:24 -0300

    1.0

---
+++ AntiSizeofMutations/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Feli.AntiSizeofMutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anti sizeof mutations, by Feli (https://github.com/01-Feli/AntiSizeofMutations)");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: drag and drop you file with sizeof mutations");
+                Console.ReadKey();
+                return;
+            }
+
+            string path = args[0].Replace("\"", "");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("The file: " + path + " doesn't exist");
+                Console.ReadKey();
+                return;
+            }
+
+            int sizeofCount = 0;
+
+            ModuleDef module = ModuleDefMD.Load(path);
+
+            foreach (var type in module.GetTypes())
+            {
+                foreach (var method in type.Methods)
+                {
+                    // Checks if the method has a body
+                    if (!method.HasBody)
+                        continue;
+
+                    for (int i = 0; i < method.Body.Instructions.Count; i++)
+                    {
+                        var instruction = method.Body.Instructions[i];
+
+                        Console.WriteLine(instruction.OpCode.ToString());
+
+                        // check if it is sizeof opCode
+                        if (instruction.OpCode != OpCodes.Sizeof)
+                            continue;
+                        //gets the value of the sizeof using the operand type
+                        int value = GetSize(instruction.Operand.ToString());
+
+                        //here i replace the opCode to the int opCode and i set the new value
+                        instruction.OpCode = OpCodes.Ldc_I4;
+                        instruction.Operand = value;
+
+                        method.Body.Instructions.OptimizeMacros();
+                        sizeofCount++;
+                    }
+                }
+            }
+
+            module.Write(path + ".removedSizeof.exe");
+
+            Console.WriteLine("Replaced " + sizeofCount + " mutations");
+            Console.ReadKey();
+        }
+
+        static int GetSize(string tipo)
+        {
+            switch (tipo)
+            {
+                case "System.SByte":
+                    return 1;
+                case "System.Boolean":
+                    return 1;
+                case "System.Byte":
+                    return 1;
+                case "System.Int16":
+                    return 2;
+                case "System.Int32":
+                    return 4;
+                case "System.Single":
+                    return 4;
+                case "System.Int64":
+                    return 8;
+                case "System.Decimal":
+                    return 16;
+            }
+            return 0;
+        }
+    }
+}
---
+++ AntiSizeofMutations/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("AntiSizeofMutations")]
+[assembly: AssemblyDescription("Anti Sizeof mutations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntiSizeofMutations")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bd50a57d-e932-45ea-bfa9-5bdbb209ab11")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
